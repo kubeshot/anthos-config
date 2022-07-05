7ac8ea7ab859841c245159253308cd361574c312
jsapkota <36951385+jsapkota@users.noreply.github.com>
2022-07-05 16:30:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,13 @@ member  = "serviceAccount:config-connector@${var.project_id}.iam.gserviceaccount
   depends_on         = [google_service_account.cluster_service_account]
 }
 
+resource "google_project_iam_member" "config-connector-compute-admin-role" {
+project = var.project_id
+role    = "roles/resourcemanager.organizationAdmin"
+member  = "serviceAccount:config-connector@${var.project_id}.iam.gserviceaccount.com"
+  depends_on         = [google_service_account.cluster_service_account]
+}
+
 #Iam binding for config_connector with cloud 
 resource "google_service_account_iam_member" "iam_binding_for_config_connector" {
   service_account_id = google_service_account.cluster_service_account.name
@@ -29,31 +36,31 @@ resource "google_service_account_iam_member" "iam_binding_for_config_connector" 
 
 #for namespace SA
 
-resource "google_service_account" "project-a_service_account" {
-  account_id   = "project-a"
+resource "google_service_account" "hub-project_service_account" {
+  account_id   = "hub-project"
   display_name = "config connector SA"
   project      = var.project_id
 }
 
-resource "google_project_iam_member" "project-a-owner-role" {
+resource "google_project_iam_member" "hub-project-owner-role" {
 project = var.project_id
 role    = "roles/owner"
-member  = "serviceAccount:project-a@${var.project_id}.iam.gserviceaccount.com"
-  depends_on         = [google_service_account.project-a_service_account]
+member  = "serviceAccount:hub-project@${var.project_id}.iam.gserviceaccount.com"
+  depends_on         = [google_service_account.hub-project_service_account]
 }
 
-resource "google_project_iam_member" "project-a-sa-metricWriter-role" {
+resource "google_project_iam_member" "hub-project-sa-metricWriter-role" {
 project = var.project_id
 role    = "roles/monitoring.metricWriter"
-member  = "serviceAccount:project-a@${var.project_id}.iam.gserviceaccount.com"
-depends_on         = [google_service_account.project-a_service_account]
+member  = "serviceAccount:hub-project@${var.project_id}.iam.gserviceaccount.com"
+depends_on         = [google_service_account.hub-project_service_account]
 } 
 
 resource "google_service_account_iam_member" "iam_binding_for_project-a_sa" {
-  service_account_id = google_service_account.project-a_service_account.name
+  service_account_id = google_service_account.hub-project_service_account.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${var.project_id}.svc.id.goog[cnrm-system/cnrm-controller-manager-project-a]"
-  depends_on         = [google_service_account.project-a_service_account]
+  member             = "serviceAccount:${var.project_id}.svc.id.goog[cnrm-system/cnrm-controller-manager-hub-project]"
+  depends_on         = [google_service_account.hub-project_service_account]
 }
 
 
